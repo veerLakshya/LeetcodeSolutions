209aaf48c4c0b69f1339bbd39a865d130946614f
veerLakshya <109882583+veerLakshya@users.noreply.github.com>
2024-01-17 20:42:58 +0530

    Time: 2 ms (65.84%), Space: 8.6 MB (41.08%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -1,29 +1,19 @@
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
-        vector <int> v;
-        sort(arr.begin(),arr.end());
-
-        int prev = arr[0];
-        int count= 0;
-        for(int i=0;i<arr.size();i++)
-        {
-            if(arr[i] == prev)
-            count++;
-            else
-            {
-                v.push_back(count);
-                count=1;
-            }
-            prev = arr[i];
-        }
-        v.push_back(count);
+        unordered_map <int,int> mp;
+        for(auto i:arr)
+            mp[i]++;
         
-        sort(v.begin(),v.end());
-
-        for(int i=1;i<v.size();i++)
-            if(v[i]==v[i-1]) return false;
-
+        unordered_set <int> v;
+        for(auto i:mp)
+        {
+            if(v.find(i.second)!=v.end())
+                return false;
+            else 
+                v.insert(i.second);
+        }
         return true;
+        
     }
 };
