035eba8dbc75b92f60740a900143eabc24e6a428
veerLakshya <109882583+veerLakshya@users.noreply.github.com>
2024-05-11 14:05:27 +0530

    Time: 0 ms (100.00%) | Memory: 7.2 MB (22.88%) - LeetSync

---
+++ 319-bulb-switcher/bulb-switcher.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int bulbSwitch(int n) {
+        // int cnt=0;
+        // for(int i=1;i<=n;i++){
+        //     if(sqrt(i)*sqrt(i)==i)cnt++;
+        // }
+        return sqrt(n);
+    }
+};
