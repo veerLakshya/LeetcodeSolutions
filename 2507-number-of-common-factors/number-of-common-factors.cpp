12b4c6c90fa5235427bffffcaf2bde50a571c304
veerLakshya <109882583+veerLakshya@users.noreply.github.com>
2024-05-12 11:49:04 +0530

    Time: 2 ms (51.22%) | Memory: 6.9 MB (95.91%) - LeetSync

---
+++ 2507-number-of-common-factors/number-of-common-factors.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int commonFactors(int a, int b) {
+        int ans=1;
+        for(int i=2;i<=min(a,b);i++){
+            if(a%i==0 && b%i==0)ans++;
+        }
+        return ans;
+    }
+};
