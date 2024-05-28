3c7425d2808bc5dd8642042768e2299831fad338
veerLakshya <109882583+veerLakshya@users.noreply.github.com>
2024-05-28 22:37:51 +0530

    Time: 7 ms (59.75%) | Memory: 12.7 MB (8.81%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -12,15 +12,12 @@ public:
         int m=grid[0].size();
         vector<vector<int>>dp(n+1,vector<int> (m+1,-1));
         dp[n-1][m-1]=grid[n-1][m-1];
-        for(int i=n-2;i>=0;i--){
-            dp[i][m-1]=grid[i][m-1]+dp[i+1][m-1];
-        }
-        for(int i=m-2;i>=0;i--){
-            dp[n-1][i]=grid[n-1][i]+dp[n-1][i+1];
-        }
-        for(int i=n-2;i>=0;i--){
-            for(int j=m-2;j>=0;j--){
-                dp[i][j]=min(dp[i][j+1],dp[i+1][j])+grid[i][j];
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                if(i==n-1 && j==m-1)continue;
+                else if(i==n-1)dp[i][j]= grid[i][j]+dp[i][j+1];
+                else if(j==m-1)dp[i][j]= grid[i][j]+dp[i+1][j];
+                else dp[i][j]= grid[i][j]+min(dp[i][j+1],dp[i+1][j]);
             }
         }
         return dp[0][0];
