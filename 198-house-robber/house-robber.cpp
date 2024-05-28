8143f2746985a290b972d19b78f2aa9190c28064
veerLakshya <109882583+veerLakshya@users.noreply.github.com>
2024-05-28 23:44:59 +0530

    Time: 4 ms (31.88%) | Memory: 9.2 MB (34.55%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -4,22 +4,22 @@ public:
         int n=nums.size();
         if(n==1)return nums[0];
         // state
-        // dp[i] = max sum from ith element to last element 
+        // dp[i] = max sum from 0th element to ith element 
         vector<int> dp(n);
         
         // transition
-        // dp[i] = max(nums[i] + dp[i+2], dp[i+1])
+        // dp[i] = max(nums[i] + dp[i-2], dp[i-1])
 
         // base case
-        // dp[n-1] = arr[n-1], dp[n-2] = max(arr[n-1],arr[n-2])
-        dp[n-1]=nums[n-1];
-        dp[n-2]=max(nums[n-1],nums[n-2]);
+        // dp[0] = arr[0], dp[1] = max(arr[0],arr[1])
+        dp[0]=nums[0];
+        dp[1]=max(nums[1],nums[0]);
 
-        for(int i=n-3;i>=0;i--){
-            dp[i]= max(nums[i]+dp[i+2], dp[i+1]);
+        for(int i=2;i<n;i++){
+            dp[i]= max(nums[i]+dp[i-2], dp[i-1]);
         }
         // final subproblem
         // dp[0] = max sum from 0th element to last element
-        return dp[0];
+        return dp[n-1];
     }
 };
