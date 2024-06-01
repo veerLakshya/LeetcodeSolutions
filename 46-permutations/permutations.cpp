65e859703ad34383d2ee199b5ea434b9745f41e2
veerLakshya <109882583+veerLakshya@users.noreply.github.com>
2024-06-02 04:42:33 +0530

    Time: 3 ms (54.05%) | Memory: 9.1 MB (54.71%) - LeetSync

---
+++ 46-permutations/permutations.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int n;
+    vector<vector<int>> ans;
+    void helper(vector<int>& temp, vector<int>& nums,vector<int>& visited){
+        if(temp.size()==n){
+            ans.push_back(temp);
+            return;
+        }
+        // if(!visited[i]){
+        //     visited[i]=
+        //     temp.push_back(nums[i]);
+        //     helper(i+1,temp,nums);
+        // temp.pop_back();
+        // }
+        for(int i=0;i<n;i++){
+            if(visited[i])continue;
+            temp.push_back(nums[i]);
+            visited[i]=1;
+            helper(temp,nums,visited);
+            temp.pop_back();
+            visited[i]=0;
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        n=nums.size();
+        vector<int> temp;
+        vector<int> visited(n,0);
+        helper( temp,nums,visited);
+        return ans;
+    }
+};
