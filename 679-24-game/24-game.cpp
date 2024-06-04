063bcade9f21e053f1b67b089c0a059b29ae47cf
veerLakshya <109882583+veerLakshya@users.noreply.github.com>
2024-06-05 03:00:17 +0530

    Time: 184 ms (5.02%) | Memory: 82.6 MB (5.02%) - LeetSync

--- 679-24-game/24-game.cpp
+++ 679-24-game/24-game.cpp
@@ -37,45 +37,6 @@ public:
                     s.insert(values[j]/values[i]);
                     ans|=helper(s);
                 }
-                // multiset<long double> temp = st;
-                // temp.erase(temp.find(values[i]));
-                // temp.erase(temp.find(values[j]));
-                // temp.insert(values[i] + values[j]);
-                // ans |= helper(temp);
-
-                // temp = st;
-                // temp.erase(temp.find(values[i]));
-                // temp.erase(temp.find(values[j]));
-                // temp.insert(values[i]*values[j]);
-                // ans |= helper(temp);
-
-                // temp = st;
-                // temp.erase(temp.find(values[i]));
-                // temp.erase(temp.find(values[j]));
-                // temp.insert(values[i] - values[j]);
-                // ans |= helper(temp);
-
-                // temp = st;
-                // temp.erase(temp.find(values[i]));
-                // temp.erase(temp.find(values[j]));
-                // temp.insert(values[j] - values[i]);
-                // ans |= helper(temp);
-
-                // temp = st;
-                // temp.erase(temp.find(values[i]));
-                // temp.erase(temp.find(values[j]));
-                // if (values[j]) {
-                //     temp.insert(values[i] / values[j]);
-                //     ans |= helper(temp);
-                // }
-
-                // temp = st;
-                // temp.erase(temp.find(values[i]));
-                // temp.erase(temp.find(values[j]));
-                // if (values[i]) {
-                //     temp.insert(values[j] / values[i]);
-                //     ans |= helper(temp);
-                // }
             }
         }
             return ans;
@@ -87,59 +48,3 @@ public:
         
     }
 };
-/*
-public:
-        for (int i = 0; i < values.size(); i++) {
-            for (int j = i + 1; j < values.size(); j++) {
-                multiset<long double> temp = s;
-                temp.erase(temp.find(values[i]));
-                temp.erase(temp.find(values[j]));
-                temp.insert(values[i] + values[j]);
-                kitna |= fn(temp);
-
-                temp = s;
-                temp.erase(temp.find(values[i]));
-                temp.erase(temp.find(values[j]));
-                temp.insert(values[i]*values[j]);
-                kitna |= fn(temp);
-
-                temp = s;
-                temp.erase(temp.find(values[i]));
-                temp.erase(temp.find(values[j]));
-                temp.insert(values[i] - values[j]);
-                kitna |= fn(temp);
-
-                temp = s;
-                temp.erase(temp.find(values[i]));
-                temp.erase(temp.find(values[j]));
-                temp.insert(values[j] - values[i]);
-                kitna |= fn(temp);
-
-                temp = s;
-                temp.erase(temp.find(values[i]));
-                temp.erase(temp.find(values[j]));
-                if (values[j]) {
-                    temp.insert(values[i] / values[j]);
-                    kitna |= fn(temp);
-                }
-
-                temp = s;
-                temp.erase(temp.find(values[i]));
-                temp.erase(temp.find(values[j]));
-                if (values[i]) {
-                    temp.insert(values[j] / values[i]);
-                    kitna |= fn(temp);
-                }
-            }
-        }
-        return kitna;
-    }
-    bool judgePoint24(vector<int>& cards) {
-        multiset<long double> s;
-        for (auto i : cards) s.insert(i);
-        fn(s);
-        if(saare.find(24)!=saare.end()) return 1;
-        return 0;
-    }
-};
-*/
