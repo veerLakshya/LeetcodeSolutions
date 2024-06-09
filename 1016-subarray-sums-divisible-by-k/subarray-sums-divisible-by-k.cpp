e79df3a37abd0f915d96f803db94b4ab62f4943b
Lakshya Veer Singh <109882583+veerLakshya@users.noreply.github.com>
2024-06-09 06:38:47 +0530

    Time: 16 ms (99.62%) | Memory: 32.5 MB (86.55%) - LeetSync

--- 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.cpp
+++ 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int subarraysDivByK(vector<int>& nums, int k) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
         int n = nums.size();
         vector<int> remainder(k,0);
         remainder[0] = 1;
