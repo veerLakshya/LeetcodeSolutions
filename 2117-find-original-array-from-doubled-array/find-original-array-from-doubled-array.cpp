e15348bdee7a6b9ad9174f303be20c7b600be4df
Lakshya Veer Singh <109882583+veerLakshya@users.noreply.github.com>
2024-06-11 12:27:04 +0530

    Time: 496 ms (7.26%) | Memory: 198.9 MB (5.59%) - LeetSync

---
+++ 2117-find-original-array-from-doubled-array/find-original-array-from-doubled-array.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> findOriginalArray(vector<int>& changed) {
+        vector<int> ans;
+        if(changed.size()==0 || changed.size()==1 || changed.size()%2)return ans;
+        multiset<int> pos,neg;
+        bool flag = 0;
+        for(int i=0;i<changed.size();i++){
+            if(changed[i]>=0)pos.insert(changed[i]);
+            else if(changed[i]<0) neg.insert(changed[i]);
+        }
+        while(pos.size()){
+            // for(auto i:pos)cout<<i<<" ";
+            int x = *pos.begin();
+            pos.erase(pos.find(x));
+            if(pos.find(2*x)==pos.end()){
+                return {};
+            }
+            ans.push_back(x);
+            pos.erase(pos.find(x*2));
+        }
+        while(neg.size()){
+            int x = *pos.end();
+            pos.erase(pos.find(x));
+            if(pos.find(2*x)==pos.end()){
+                return {};
+            }
+            ans.push_back(x);
+            pos.erase(pos.find(x*2));
+        }
+        return ans;
+        
+    }
+};
