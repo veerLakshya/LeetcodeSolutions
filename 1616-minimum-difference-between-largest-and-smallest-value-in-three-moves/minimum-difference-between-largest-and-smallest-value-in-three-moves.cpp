800a88e66b3b26be8b227e9031e89e326d6ed128
Lakshya Veer Singh <109882583+veerLakshya@users.noreply.github.com>
2024-07-03 22:27:54 +0530

    Time: 72 ms (39.46%) | Memory: 38.7 MB (50.84%) - LeetSync

--- 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
+++ 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
@@ -6,8 +6,6 @@ public:
         if(n<=4){
             return 0;
         }
-        for(auto i: nums)cout<<i<<" ";
         return min(nums[n-1]-nums[3],min(nums[n-2]-nums[2],min(nums[n-3]-nums[1],nums[n-4]-nums[0])));
-    
     }
 };
