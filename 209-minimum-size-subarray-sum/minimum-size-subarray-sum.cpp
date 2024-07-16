143746792665c76ab6179c686d30926767a92229
Lakshya Veer Singh <109882583+veerLakshya@users.noreply.github.com>
2024-07-16 17:16:48 +0530

    Time: 31 ms (29.70%) | Memory: 30.8 MB (9.04%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
-        int sum = 0, ans = INT_MAX;
-        deque <int> dq;
-        for(int i = 0; i < nums.size(); i++){
-            sum += nums[i];
-            dq.push_back(nums[i]);
-            if(sum >= target){
-                do{
-                // cout<<"sum: "<<sum<<" size: "<<dq.size()<<endl;
-                    ans = min( ans, (int)dq.size() );
-                    sum -= dq.front();
-                    dq.pop_front();
-                }while(sum >= target);
+        int n = nums.size();
+        int sum = 0;
+        int ans = INT_MAX, i = 0, j =0;
+
+        while(i!=n){
+            while(sum<target && j<n){
+                sum+=nums[j];
+                j++;
             }
+            if(sum>=target)
+            ans= min(ans,j-i);
+            sum-=nums[i];
+            i++;
         }
-        return ans == INT_MAX ? 0 : ans;
+        return ans==INT_MAX?0:ans;
     }
 };
