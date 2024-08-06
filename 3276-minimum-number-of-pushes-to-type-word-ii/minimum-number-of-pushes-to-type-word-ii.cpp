920632bb37a925e47d478e7ea9099e639e901596
Lakshya Veer Singh <109882583+veerLakshya@users.noreply.github.com>
2024-08-06 17:25:11 +0530

    Time: 65 ms (89.01%) | Memory: 27.7 MB (14.43%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
@@ -1,17 +1,10 @@
 class Solution {
 public:
     int minimumPushes(string word) {
-        vector<int> arr;
-        map<char,int> mp;
-        for(auto i: word) mp[i]++;
-        for(auto i: mp) arr.push_back(i.second);
-        int ans = 0;
-        if(arr.size()<= 8){
-            return word.size();
-            return 0;
-        }
+        vector<int> arr(26);
+        for(auto i: word) arr[i-'a']++;
         sort(arr.rbegin(),arr.rend());
-        int cnt = 0;
+        int ans = 0, cnt = 0;
         for(auto i: arr){
             cnt++;
             if(cnt<=8) ans+=i;
