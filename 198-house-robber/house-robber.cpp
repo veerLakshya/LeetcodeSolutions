6c2fad5417797bead33217ee9857cc4f6a0f5a30
Lakshya Veer Singh <109882583+veerLakshya@users.noreply.github.com>
2024-08-17 18:22:59 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (39.97%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,25 +1,12 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        int n=nums.size();
-        if(n==1)return nums[0];
-        // state
-        // dp[i] = max sum from 0th element to ith element 
-        vector<int> dp(n);
-        
-        // transition
-        // dp[i] = max(nums[i] + dp[i-2], dp[i-1])
-
-        // base case
-        // dp[0] = arr[0], dp[1] = max(arr[0],arr[1])
-        dp[0]=nums[0];
-        dp[1]=max(nums[1],nums[0]);
-
-        for(int i=2;i<n;i++){
-            dp[i]= max(nums[i]+dp[i-2], dp[i-1]);
+        int n = nums.size();
+        int dp[n];
+        dp[0] = nums[0]; if(n>1) dp[1] = max(nums[0],nums[1]);
+        for(int i = 2; i < n; i++){
+            dp[i] = max(nums[i]+ dp[i-2], dp[i-1]);
         }
-        // final subproblem
-        // dp[0] = max sum from 0th element to last element
         return dp[n-1];
     }
 };
