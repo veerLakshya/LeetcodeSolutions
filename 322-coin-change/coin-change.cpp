01803659a96a5d7103ad0aa1ce138a1ab214f2ad
Lakshya Veer Singh <109882583+veerLakshya@users.noreply.github.com>
2024-09-22 10:07:56 +0530

    Time: 17 ms (92.16%) | Memory: 16.5 MB (63.17%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        vector<int> dp(amount + 1, 1e9); // initialized with very high values initially
-        // dp[k] = minimum number of coins required to construct a sum of k
-        // dp[k] = min(dp[k-ai]) + 1 : i goes from 1  to n
-        dp[0] = 0; // base case
-        for (int i = 1; i <= amount; i++) {
-            for (int j = 0; j < coins.size(); j++) {
-                if (coins[j] <= i)
-                    dp[i] = min(dp[i], dp[i - coins[j]] + 1);
+        vector<int> dp(amount+1, 1e9);
+        dp[0] = 0;
+        for(int i = 1; i <= amount; i++){
+            for(auto coin: coins){
+                if(coin <= i){
+                    dp[i] = min(dp[i], dp[i-coin] + 1);
+                }
             }
         }
-        return (dp[amount] != 1e9 ? dp[amount] : -1);
+        return (dp[amount] == 1e9 ? -1 : dp[amount]);
     }
 };
