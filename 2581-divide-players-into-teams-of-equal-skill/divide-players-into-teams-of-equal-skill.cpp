f538c6e887562ff060edc23feb648f12822e8253
Lakshya Veer Singh <109882583+veerLakshya@users.noreply.github.com>
2024-10-04 16:17:57 +0530

    Time: 316 ms (5.40%) | Memory: 93 MB (5.00%) - LeetSync

--- 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.cpp
+++ 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.cpp
@@ -1,15 +1,11 @@
 class Solution {
 public:
     long long dividePlayers(vector<int>& skill) {
-        int n = skill.size();
-        long long total = accumulate(skill.begin(),skill.end(), 0LL);
-        // if(total&1 && n>2) return -1;
-        long long target = total * 2 / n;
-
         multiset <long long> st;
         for(auto i: skill) st.insert(i);
 
         long long ans = 0;
+        long long target = accumulate(skill.begin(),skill.end(), 0LL) * 2 / skill.size();
 
         for(auto i: skill){
             if(st.find(i)!=st.end()){
@@ -23,7 +19,6 @@ public:
                     st.insert(i);
             }
         }
-        cout<<ans;
         if(st.size()) return -1;
         return ans;
     }
