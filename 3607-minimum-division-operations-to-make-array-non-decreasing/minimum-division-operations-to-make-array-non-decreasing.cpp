02e892a937bb46e448b0a62b5e3f3a3cfcf86505
Lakshya Veer Singh <109882583+veerLakshya@users.noreply.github.com>
2024-10-20 21:20:59 +0530

    Time: 267 ms (78.26%) | Memory: 141.8 MB (39.13%) - LeetSync

--- 3607-minimum-division-operations-to-make-array-non-decreasing/minimum-division-operations-to-make-array-non-decreasing.cpp
+++ 3607-minimum-division-operations-to-make-array-non-decreasing/minimum-division-operations-to-make-array-non-decreasing.cpp
@@ -1,30 +1,18 @@
 class Solution {
 public:
-    int getFactors(int n) {
-        for (int i = 2; i <= n/2; ++i) {
-            if (n % i == 0) return n / i;
+    int getFactor(int n) {
+        for (int i = 2; i * i <= n; ++i) {
+            if (n % i == 0) return i;
         }
-        return -1;
-    }
-    bool isPrime(int n) {
-        if (n <= 1) return false; 
-        if (n <= 3) return true;
-        if (n % 2 == 0 || n % 3 == 0) return false; 
-        for (int i = 5; i * i <= n; i += 6) { 
-            if (n % i == 0 || n % (i + 2) == 0) return false;
-        }
-        return true; 
+        return n;
     }
     int minOperations(vector<int>& nums) {
         int ans = 0, n = nums.size();
         for (int i = n - 2; i >= 0; i--) {
             if (nums[i] > nums[i + 1]) {
-                if (isPrime(nums[i]))
-                    return -1;
-                else{
-                    int fact = getFactors(nums[i]);   
-                    nums[i] = nums[i]/fact;
-                    if(nums[i] > nums[i+1]) return -1;
+                if (nums[i] > nums[i + 1]) {
+                    nums[i] = getFactor(nums[i]);
+                    if (nums[i] > nums[i + 1]) return -1;
                     ans++;
                 }
             }
