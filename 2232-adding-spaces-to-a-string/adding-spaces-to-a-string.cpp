f76fa6741ff9f67300a5875d8693d3e84453a093
Lakshya Veer Singh <109882583+veerLakshya@users.noreply.github.com>
2024-12-03 23:28:32 +0530

    Time: 25 ms (25.90%) | Memory: 84.4 MB (27.25%) - LeetSync

--- 2232-adding-spaces-to-a-string/adding-spaces-to-a-string.cpp
+++ 2232-adding-spaces-to-a-string/adding-spaces-to-a-string.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
     string addSpaces(string s, vector<int>& spaces) {
+        ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
         string ans = "";
-        sort(spaces.begin(),spaces.end());
+        // sort(spaces.begin(),spaces.end());
         int j = 0;
         for(int i = 0; i < s.size(); i++){
             if(j < spaces.size() && i == spaces[j]){
