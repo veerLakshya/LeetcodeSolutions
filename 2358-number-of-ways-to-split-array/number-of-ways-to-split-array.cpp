242ae955f5d72ec9b59d8b7e5c1c5c7057931442
Lakshya Veer Singh <109882583+veerLakshya@users.noreply.github.com>
2025-01-03 13:14:20 +0530

    Time: 4 ms (47.38%) | Memory: 97 MB (26.39%) - LeetSync

--- 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.cpp
+++ 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.cpp
@@ -5,8 +5,6 @@ public:
         vector<long long> pre(n+1, 0);
         for(int i = 1; i <= n; i++)
             pre[i] = pre[i-1] + nums[i-1];
-        for(auto i: pre) cout << i << " ";
-        cout << endl;
         for(int i = 1; i < n; i++) ans += (pre[i] >= pre[n] - pre[i]);
         return ans;
     }
