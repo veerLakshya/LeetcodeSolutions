25ca6c9638a46615598faad6783ef0856268b176
Lakshya Veer Singh <109882583+veerLakshya@users.noreply.github.com>
2025-01-31 00:19:09 +0530

    Time: 0 ms (100.00%) | Memory: 17.1 MB (90.97%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -3,25 +3,27 @@ public:
     bool isBipartite(vector<vector<int>>& graph) {
         int n = graph.size();
         vector<int> vis(n, -1);
-        auto bfs = [&](int cur, int col, auto self) -> bool{
-            vis[cur] = col;
-            bool ans = true;
-            for(auto nbr: graph[cur]){
-                if(vis[nbr] == -1) {
-                    bool f = self(nbr,(col == 0 ? 1 : 0), self);
-                    if(!f) return false;
+        bool is_bipartite = true;
+        queue<int> q;
+        for (int st = 0; st < n; ++st) {
+            if (vis[st] == -1) {
+                q.push(st);
+                vis[st] = 0;
+                while (!q.empty()) {
+                    int v = q.front();
+                    q.pop();
+                    for (int u : graph[v]) {
+                        if (vis[u] == -1) {
+                            vis[u] = vis[v] ^ 1;
+                            q.push(u);
+                        } else {
+                            is_bipartite &= (vis[u] != vis[v]);
+                            if(!is_bipartite)return false;
+                        }
+                    }
                 }
-                else if(vis[nbr] == col) return false;
-            }
-            return true;
-        };
-        bool ans = true;
-        for(int i = 0; i < n; i++){
-            if(vis[i] == -1){
-                bool cur = bfs(i,0, bfs);
-                ans &= cur;
             }
         }
-       return ans;
+        return true;
     }
 };
