055a0390f1461966625eed57679284f8402d9019
Lakshya Veer Singh <109882583+veerLakshya@users.noreply.github.com>
2025-02-13 21:11:19 +0530

    Time: 88 ms (86.21%) | Memory: 91.4 MB (90.45%) - LeetSync

--- 3332-minimum-operations-to-exceed-threshold-value-ii/minimum-operations-to-exceed-threshold-value-ii.cpp
+++ 3332-minimum-operations-to-exceed-threshold-value-ii/minimum-operations-to-exceed-threshold-value-ii.cpp
@@ -2,14 +2,13 @@ class Solution {
 public:
     int minOperations(vector<int>& nums, int k) {
         long long ans = 0;
-        multiset<long long> st;
-        for(auto i: nums) st.insert(i);
-        while(st.size() && * st.begin() < k){
-            long long f = *st.begin();
-            st.erase(st.find(f));
-            long long s = *st.begin();
-            st.erase(st.find(s));
-            st.insert( 2*f + s);
+        priority_queue<long long, vector<long long>, greater<long long>> pq(nums.begin(),nums.end());
+        while(pq.size() && pq.top() < k){
+            long long x = pq.top();
+            pq.pop();
+            long long y = pq.top();
+            pq.pop();
+            pq.push(2*x + y);
             ans++;
         }
         return ans;
