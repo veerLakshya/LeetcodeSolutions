659da392348eee17b196074dbdef9fd0aa76e4f4
Lakshya Veer Singh <109882583+veerLakshya@users.noreply.github.com>
2025-02-18 11:55:53 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (78.36%) - LeetSync

--- 100-same-tree/same-tree.cpp
+++ 100-same-tree/same-tree.cpp
@@ -11,29 +11,12 @@
  */
 class Solution {
 public:
-    // bool f(TreeNode* p, TreeNode* q){
-    //     if(p->val != q->val) return false;
-
-    //     if((p->left == NULL && q->left == NULL) && (p->right == NULL && q->right == NULL)) return true;
-    //     return true;
-    // }
-    void dfs(TreeNode* x, vector<int>& a){
-        if(x == nullptr){
-            a.push_back(-1e6);
-            return;
-        }
-        a.push_back(x->val);
-        dfs(x->left, a);
-        dfs(x->right, a);
-    }
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        vector<int> a,b;
-        dfs(p,a);
-        dfs(q,b);
-        // for(auto i: a) cout<<i<<" ";
-        // cout<<endl;
-        // for(auto i: b) cout<<i<<" ";
-        return a==b;
-        // return f(p, q);
+        if(!p && !q) return true;
+        if(!p || !q) return false;
+        if(p->val != q->val) return false;
+        bool l = isSameTree(p->left , q->left);
+        bool r = isSameTree(p->right , q->right);
+        return l&r;
     }
 };
