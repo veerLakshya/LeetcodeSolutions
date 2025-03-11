ce14132dae2ce177430566942cf8972fd8c101a8
Lakshya Veer Singh <109882583+veerLakshya@users.noreply.github.com>
2025-03-11 14:58:37 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (41.14%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
-    int n;
-    int f(int i,int flag,vector<int> &nums,vector<vector<int>> &dp){
-        if(i >= n) return 0;
-        if(i == n - 1) return flag == 1 ? 0 : nums[n - 1];
-        if(dp[i][flag] != -1) return dp[i][flag];
-        int a1 = f(i + 1,flag,nums,dp);
-        int a2 = nums[i] + f(i + 2,(i == 0 ? 1 : flag),nums,dp);
-
-        return dp[i][flag] = max(a2,a1);
-    }
     int rob(vector<int>& nums) {
-        n = nums.size();
-        vector<vector<int>> dp(n,vector<int>(2,-1));
-        return f(0,0,nums,dp);
+        int ans1 = 0, ans2 = 0, n = nums.size();
+        if(n <= 3) return *max_element(nums.begin(),nums.end());
+
+        vector<int> dp(n, 0);
+        dp[0] = nums[0];
+        dp[1] = max(nums[0],nums[1]);
+        for(int i = 2; i < n - 1; i++){
+            dp[i] = max(dp[i-2] + nums[i], dp[i-1]);
+        }
+        ans1 = dp[n-2];
+        dp.assign(n,0);
+        dp[1] = nums[1];
+        dp[2] = max(nums[1],nums[2]);
+        for(int i = 3; i < n; i++){
+            dp[i] = max(dp[i-2] + nums[i], dp[i-1]);
+        } 
+        ans2 = dp[n-1];
+        return max(ans1, ans2);
     }
 };
