1380d96095b372ca8fc10b1b1402e41eac5539ca
Lakshya Veer Singh <109882583+veerLakshya@users.noreply.github.com>
2025-03-16 10:45:42 +0530

    Time: 70 ms (21.66%) | Memory: 59.4 MB (45.76%) - LeetSync

--- 2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.cpp
+++ 2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
     long long repairCars(vector<int>& ranks, int cars) {
-        long long low=0;
-        long long high=1e14;
-        long long ans=0;
-        while(low<=high){
-            long long mid=(low+high)/2;
-            long long cnt=0;
-            for(auto i:ranks){
-                cnt+=sqrt(mid/i);
+        long long  n = ranks.size(), l = 0, r = 1e16, ans = 1e16;
+
+        while(l <= r){
+            long long  mid = (l+r)/2;
+            long long  cnt = 0;
+            for(long long  i = 0; i < n; i++){
+                cnt += sqrt(mid / ranks[i]);
             }
-            if(cnt>=cars){
-                ans=mid;
-                high=mid-1;
+            if(cnt >= cars){
+                ans = mid;
+                r = mid - 1;
             }
-            else
-                low=mid+1;
-        }
+            else l = mid + 1;
+        } 
         return ans;
-        
     }
 };
