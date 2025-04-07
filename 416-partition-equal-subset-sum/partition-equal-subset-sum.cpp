78f1fcc0373c2056523396983343b58e4da88bef
Lakshya Veer Singh <109882583+veerLakshya@users.noreply.github.com>
2025-04-07 23:48:27 +0530

    Time: 143 ms (32.30%) | Memory: 30.7 MB (52.29%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     int dp[201][20001];
-    bool h(int ind, int sum, vector<int>& a){
-        if(sum == 0) return 1;
-        if(ind < 0 || sum < 0) return 0;
-        if(dp[ind][sum] != -1) return dp[ind][sum];
-        bool take = h(ind - 1, sum - a[ind], a);
-        bool nottake = h(ind -1, sum, a);
-        return dp[ind][sum] = take||nottake;
+    int n, total;
+    bool helper(int ind, int cur, auto& nums){
+        if(cur == total) return 1;
+        if(ind == n) return 0;
+        if(dp[ind][cur] != -1) return dp[ind][cur];
+        return dp[ind][cur] = helper(ind + 1, cur + nums[ind], nums)||helper(ind + 1, cur, nums); 
     }
     bool canPartition(vector<int>& nums) {
-        int sum = accumulate(nums.begin(), nums.end(), 0);
-        if(sum&1) return 0;
+        n = nums.size(), total = accumulate(nums.begin(), nums.end(), 0);
         memset(dp, -1, sizeof dp);
-        return h(nums.size()-1, sum/2, nums);
+        if(total%2)return 0;
+        total /= 2;
+        return helper(0, 0, nums);
     }
 };
