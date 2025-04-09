717069f48c158aa619b7a303bf5e2983fad89d71
Lakshya Veer Singh <109882583+veerLakshya@users.noreply.github.com>
2025-04-09 23:44:22 +0530

    Time: 146 ms (10.60%) | Memory: 144.9 MB (5.05%) - LeetSync

--- 2419-subarray-with-elements-greater-than-varying-threshold/subarray-with-elements-greater-than-varying-threshold.cpp
+++ 2419-subarray-with-elements-greater-than-varying-threshold/subarray-with-elements-greater-than-varying-threshold.cpp
@@ -1,67 +1,55 @@
-struct DSU {
-    int mx;
+struct DSU{
+    int mx = 1;
     vector<int> par, size;
-    
-    DSU (int n) {
-        mx = 1;
+    DSU(int n){
         par.resize(n+1);
         size.resize(n+1, 1);
-        
-        for (int j = 1; j <= n; j ++) par[j] = j;
+        iota(par.begin(),par.end(), 0);
     }
-    
-    int Leader (int x) {
-        if (par[x] == x) return x;
-        return (par[x] = Leader (par[x]));
+    int findPar(int x){
+        if(par[x] == x) return x;
+        return par[x] = findPar(par[x]);
     }
-    
-    void merge (int x, int y) {
-        x = Leader(x);
-        y = Leader(y);
-        
-        if (x == y) return;
-        if (size[x] > size[y]) swap (x, y);
-        
+    void merge(int x, int y){
+        x = findPar(x);
+        y = findPar(y);
+
+        if(x == y) return;
+        if(size[x] > size[y]) swap(x, y);
+
         size[y] += size[x];
         par[x] = y;
-        
+
         mx = max (mx, size[y]);
     }
 };
-
 class Solution {
 public:
     int validSubarraySize(vector<int>& nums, int threshold) {
-        int n = nums.size();
-        
-        vector<pair<double, int>> val_and_ind;
-        for (int j = 0; j < n; j ++) {
-            val_and_ind.push_back({nums[j], j+1});           
+        int n = nums.size(), k = 1;
+
+        vector<pair<double,int>> p; // val, ind
+        for(int i = 0; i < n; i++){
+            p.push_back({nums[i], i + 1});
         }
-        
-        sort (val_and_ind.begin(), val_and_ind.end());
-        
-        vector<bool> active (n+1, false);
-        int k = 1;
+
+        sort(p.rbegin(), p.rend());
+
+        vector<bool> active(n+1, 0);
         DSU dsu(n);
-        
-        for (int j = n-1; j >= 0 && k <= n;) {
-            double k_threshold = (double)threshold/k;
-            
-            while (j >= 0 && val_and_ind[j].first > k_threshold) {
-                int ind = val_and_ind[j].second;
-                active[ind] = true;
-                
-                if (active[ind-1]) dsu.merge (ind, ind-1);
-                if (active[ind+1]) dsu.merge (ind, ind+1);
-                
-                j --;
+
+        for(int i = 0; i < n && k <= n;){
+            double newt = (double)(threshold/k);
+            while(i < n && p[i].first > newt){
+                int ind = p[i].second;
+                active[ind] = 1;
+                if(active[ind-1]) dsu.merge(ind, ind-1);
+                if(active[ind+1]) dsu.merge(ind, ind+1);
+                i++;
             }
-                        
-            if (j < n-1 && dsu.mx >= k) return k;
-            k ++;
+            if(i > 0 && dsu.mx >= k) return k;
+            k++;
         }
-        
         return -1;
     }
 };
