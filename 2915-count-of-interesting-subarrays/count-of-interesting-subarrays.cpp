57c036a76bfd6f9c024480bd4d289d971b0e0ec4
Lakshya Veer Singh <109882583+veerLakshya@users.noreply.github.com>
2025-04-25 09:46:09 +0530

    Time: 58 ms (55.19%) | Memory: 127.4 MB (8.74%) - LeetSync

--- 2915-count-of-interesting-subarrays/count-of-interesting-subarrays.cpp
+++ 2915-count-of-interesting-subarrays/count-of-interesting-subarrays.cpp
@@ -2,17 +2,13 @@ class Solution {
 public:
     long long countInterestingSubarrays(vector<int>& nums, int modulo, int k) {
         long long ans = 0, n = nums.size();
-        for(auto &i: nums) i = (i%modulo == k);
         vector<int> pre(n + 1, 0);
-        for(int i = 1; i <= n; i++) pre[i] = pre[i-1] + nums[i-1];
-        for(auto i: nums) cout << i << " ";
-        cout << endl;
-        for(auto i: pre) cout << i << " ";
-        map<int,int> mp;
+        for(int i = 1; i <= n; i++) pre[i] = pre[i-1] + (nums[i-1]%modulo==k);
+        unordered_map<int,int> mp;
         mp[pre[0]] = 1;
         for(int i = 1; i <= n; i++){
             // how many indices such that (pre[i] - pre[j])%mod == k
-            // pre[i]%mod - pre[j]%mod 
+            // pre[i] - pre[j] = k%mod
             ans += mp[(pre[i]-k)%modulo];
             mp[pre[i]%modulo]++;
         }
