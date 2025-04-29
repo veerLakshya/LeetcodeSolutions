550e1d0e56523292beddbdef35c2f62adc58c392
Lakshya Veer Singh <109882583+veerLakshya@users.noreply.github.com>
2025-04-29 21:01:10 +0530

    Time: 0 ms (100.00%) | Memory: 23.5 MB (18.23%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -1,22 +1,11 @@
 class Solution {
 public:
     int candy(vector<int>& ratings) {
-        int n=ratings.size();
-        vector<int> candies(n,1);
-        for(int i=1;i<n;i++){
-            if(ratings[i]>ratings[i-1])candies[i]=candies[i-1]+1;
-        }
-        for(auto i:candies)cout<<i<<" ";
-        cout<<endl;
-        for(int i=n-2;i>=0;i--){
-            if(ratings[i]>ratings[i+1]){
-                if(candies[i]<=candies[i+1])
-                candies[i]=candies[i+1]+1;;
-            }
-        }
-        int ans=0;
-        for(auto i:candies)cout<<i<<" ";
-        for(int i=0;i<n;i++)ans+=candies[i];
+        int n = ratings.size(), ans = 0;
+        vector<int> l(n, 1), r(n, 1);
+        for(int i = 1; i < n; i++) l[i] = 1 + (ratings[i] > ratings[i-1] ? l[i-1] : 0);
+        for(int i = n - 2; i >= 0; i--) r[i] = 1 + (ratings[i] > ratings[i+1] ? r[i+1]: 0);
+        for(int i = 0; i < n; i++) ans += max(l[i], r[i]);
         return ans;
     }
 };
