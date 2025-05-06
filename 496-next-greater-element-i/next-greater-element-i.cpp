e216ad6119796e1b378cd0a0eb9345135eb61ea2
Lakshya Veer Singh <109882583+veerLakshya@users.noreply.github.com>
2025-05-06 05:40:29 +0530

    Time: 3 ms (43.26%) | Memory: 19.9 MB (13.20%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -6,13 +6,8 @@ public:
         stack<int> st;
         st.push(nums2[n-1]);
         for(int i = n - 2; i >= 0; i--){
-            if(st.size() && st.top() > nums2[i]){
-                a[nums2[i]] = st.top();
-            }
-            else{
-                while(st.size() && st.top() < nums2[i]) st.pop();
-                if(st.size()) a[nums2[i]] = st.top();
-            }
+            while(st.size() && st.top() < nums2[i]) st.pop();
+            if(st.size()) a[nums2[i]] = st.top();
             st.push(nums2[i]);
         }
         for(auto i: nums1){
