15b8d4db8c26fa99f38c374c17cdd272152c8622
Lakshya Veer Singh <109882583+veerLakshya@users.noreply.github.com>
2025-05-07 15:59:21 +0530

    Time: 7 ms (98.47%) | Memory: 79.4 MB (92.38%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -4,13 +4,14 @@ public:
         int n = heights.size();
         int ans = 0;
         stack<int> st;
+        // for each pillar we can take width till its left min and its right min
         for(int i = 0; i <= n; i++){
             while(st.size() && (i == n || heights[st.top()] >= heights[i])){
                 int ind = st.top();
                 st.pop();
-                int l = (st.size() ? st.top() : - 1);
+                int l = (st.size() ? st.top() + 1 : 0);
                 int r = i;
-                ans = max(ans, heights[ind]*(r - l - 1));
+                ans = max(ans, heights[ind]*(r - l));
             }
             st.push(i);
         }
