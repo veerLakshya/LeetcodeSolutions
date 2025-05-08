4bd8647b3694bd1169f39b6c810e0a591f95ba6e
Lakshya Veer Singh <109882583+veerLakshya@users.noreply.github.com>
2025-05-08 14:41:53 +0530

    Time: 1071 ms (5.00%) | Memory: 38.2 MB (5.19%) - LeetSync

---
+++ 221-maximal-square/maximal-square.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    void print(auto& a) {
+        for (auto i : a) {
+            for (auto j : i)
+                cout << j << " ";
+            cout << endl;
+        }
+        cout << endl;
+    }
+    int maximalSquare(vector<vector<char>>& matrix) {
+        int n = matrix.size(), m = matrix[0].size(), ans = 0;
+        vector<vector<int>> a(n, vector<int> (m, 0));
+        for(int j = 0; j < m; j++) a[0][j] = (matrix[0][j] == '1');
+
+        for(int j = 0; j < m; j++){
+            for(int i = 1; i < n; i++){
+                if(matrix[i][j] == '0') continue;
+                else a[i][j] = 1 + a[i-1][j];
+            }
+        }
+        print(a);
+        for(int i = 0; i < n; i++){
+            stack<int> st;
+            for(int j = 0; j <= m; j++){
+                while(st.size() && (j == m || a[i][st.top()] > a[i][j])){
+                    int mid = st.top();
+                    st.pop();
+                    int l = (st.size() ? st.top() + 1 : 0);
+                    int r = j;
+                    int side = min((r - l), a[i][mid]);
+                    // cout << r - l << " " << a[i][mid] << endl;;
+                    // cout << mid << " smaller on:"<< l << endl;
+                    ans = max(ans, side*side);
+                }
+                st.push(j);
+            }
+            // cout << endl;
+        }
+        return ans;
+    }
+};
