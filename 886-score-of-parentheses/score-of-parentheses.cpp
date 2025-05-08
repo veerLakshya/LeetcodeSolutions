55f88dc8d07b65ea4608f94a1d5bb01e992cc479
Lakshya Veer Singh <109882583+veerLakshya@users.noreply.github.com>
2025-05-08 15:10:18 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (65.91%) - LeetSync

--- 886-score-of-parentheses/score-of-parentheses.cpp
+++ 886-score-of-parentheses/score-of-parentheses.cpp
@@ -5,6 +5,7 @@ public:
         for(int i = 0; i < s.size(); i++){
             if(s[i] == '(') cur++;
             else cur--;
+            // whenever we find a () we add 2^# of outerlayers to our answer
             if(s[i] == ')' && s[i-1] == '(') ans += 1 << cur;
         }
         return ans;
